c7058509c33e6b83465855d3e23818e58f9249f7
wagner <wagner.dev1337@gmail.com>
2019-11-08 23:36:18 -0300

    modelando mais json

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,18 +1,22 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+    belongs_to :kind#, optional: true
 
-    def author
-        "Wagner Abrantes"
-    end
+        
+    #end
+    #def author
+     #   "Wagner Abrantes"
+    #end
 
-    def kind_description
-        self.kind.description
-    end
+    #def kind_description
+     #   self.kind.description
+    #end
 
-    def as_json(options={})
-        super(
-            root: true,
-            methods: [ :kind_description, :author ]
-        )
-    end
+    #def as_json(options={})
+       # super(
+      #      root: true,
+     #       methods: [ :kind_description, :author ]
+    #    )
+   # end
+
+  
 end
